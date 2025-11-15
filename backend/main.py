5c8e205750d7b37de07600e4a55b98f5bb80dafe
Bhawana <bhwbi.rawal@gmail.com>
2025-11-15 13:40:54 +0545

    add organization models and its endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
+from database import create_db_and_tables
+from routers.organization_router import router as organization_router
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(organization_router)
