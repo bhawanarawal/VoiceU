02ca01c5bf796b76b10b676702ffc637e06133e8
Bhawana <bhwbi.rawal@gmail.com>
2025-11-19 12:37:19 +0545

    add position_schema

---
+++ backend/schemas/position_schema.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class PositionCreate(BaseModel):
+    position_name: str
+    description: Optional[str] = None
+    max_candidates: Optional[int] = None
+
+class PositionRead(BaseModel):
+    position_id: int
+    position_name: str
+    description: Optional[str] = None
+    max_candidates: Optional[int] = None
+
+    model_config = {
+        "from_attributes": True 
+    }
