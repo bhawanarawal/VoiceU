588943d322482744c91aefbc0332f8e437ec4b83
Bhawana <bhwbi.rawal@gmail.com>
2025-11-24 12:35:08 +0545

    modify election model

--- backend/models/election.py
+++ backend/models/election.py
@@ -6,13 +6,13 @@ def utc_now():
     return datetime.now(timezone.utc)
 
 class Election(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    org_id: int = Field(foreign_key="organization.id")
+    election_id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int #= Field(foreign_key="user.user_id")
+    affiliation_id: int = Field(foreign_key="affiliation.affiliation_id")
     election_name: str
     start_date: datetime
     end_date: datetime
     status: str
     description: Optional[str] = None
-    type: Optional[str] = None
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
